d4a87252417539548eec1e1c28c46f88acbe8fe7
nabiCoder <nabiulli_n@vk.com>
2023-11-21 17:42:19 +0500

    Initial Commit

---
+++ RxSwiftPetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftPetProject
+//
+//  Created by Денис Набиуллин on 21.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
